4c81340a45c8ac9869b849aa2f4f54dc11a73ae1
Theo Paris <theoplaysmc@gmail.com>
2020-05-13 10:12:28 -0700

    format

--- src/astar.ts
+++ src/astar.ts
@@ -1,127 +1,128 @@
-import assert from 'assert'
-import FastPriorityQueue from 'fastpriorityqueue'
+import assert from "assert";
+import FastPriorityQueue from "fastpriorityqueue";
 
 function reconstructPath(node): any[] {
-	if (node.parent !== undefined) {
-		const pathSoFar = reconstructPath(node.parent)
-		pathSoFar.push(node.data)
-		return pathSoFar
-	} else {
-		// this is the starting node
-		return [ node.data ]
-	}
+    if (node.parent !== undefined) {
+        const pathSoFar = reconstructPath(node.parent);
+        pathSoFar.push(node.data);
+        return pathSoFar;
+    } else {
+        // this is the starting node
+        return [node.data];
+    }
 }
 
 function defaultHash(node): string {
-	return node.toString()
+    return node.toString();
 }
 
 function heapComparator(a, b): boolean {
-	return a.f < b.f ? true : false
+    return a.f < b.f ? true : false;
 }
 
 export class PathfindingResult {
-  public status: string;
-  public cost: number;
-  public path: any[];
+    public status: string;
+    public cost: number;
+    public path: any[];
 
-  constructor(status: string, cost: number, path: any[]) {
-    this.status = status;
-    this.cost = cost;
-    this.path = path;
-  }
+    constructor(status: string, cost: number, path: any[]) {
+        this.status = status;
+        this.cost = cost;
+        this.path = path;
+    }
 }
 
 /**
   Credit goes to https://github.com/andrewrk/node-astar
  */
 export const aStar = (params): PathfindingResult => {
-	assert.ok(params.start !== undefined)
-	assert.ok(params.isEnd !== undefined)
-	assert.ok(params.neighbor)
-	assert.ok(params.distance)
-	assert.ok(params.heuristic)
-	if (params.timeout === undefined) params.timeout = Infinity
-	assert.ok(!isNaN(params.timeout))
-	const hash = params.hash || defaultHash
+    assert.ok(params.start !== undefined);
+    assert.ok(params.isEnd !== undefined);
+    assert.ok(params.neighbor);
+    assert.ok(params.distance);
+    assert.ok(params.heuristic);
+    if (params.timeout === undefined) params.timeout = Infinity;
+    assert.ok(!isNaN(params.timeout));
+    const hash = params.hash || defaultHash;
 
-	const startNode = {
-		data: params.start,
-		g: 0,
-		f: 0,
-		h: params.heuristic(params.start),
-	}
-	let bestNode = startNode
-	startNode.f = startNode.h
-	// leave .parent undefined
-	const closedDataSet = new Set()
-	const openHeap = new FastPriorityQueue(heapComparator)
-	const openDataMap = new Map()
-	openHeap.add(startNode)
-	openDataMap.set(hash(startNode.data), startNode)
-	const startTime = new Date()
-	while (openHeap.size) {
-		if (new Date().getTime() - startTime.getTime() > params.timeout) {
-			return {
-				status: 'timeout',
-				cost: bestNode.g,
-				path: reconstructPath(bestNode),
-			}
-		}
-		const node = openHeap.peek()
-		openDataMap.delete(hash(node.data))
-		if (params.isEnd(node.data)) {
-			// done
-			return {
-				status: 'success',
-				cost: node.g,
-				path: reconstructPath(node),
-			}
-		}
-		// not done yet
-		closedDataSet.add(hash(node.data))
-		const neighbors = params.neighbor(node.data)
-		for (let i = 0; i < neighbors.length; i++) {
-			const neighborData = neighbors[i]
-			if (closedDataSet.has(hash(neighborData))) {
-				// skip closed neighbors
-				continue
-			}
-			const gFromThisNode = node.g + params.distance(node.data, neighborData)
-			let neighborNode = openDataMap.get(hash(neighborData))
-			let update = false
-			if (neighborNode === undefined) {
-				// add neighbor to the open set
-				neighborNode = {
-					data: neighborData,
-				}
-				// other properties will be set later
-				openDataMap.set(hash(neighborData), neighborNode)
-			} else {
-				if (neighborNode.g < gFromThisNode) {
-					// skip this one because another route is faster
-					continue
-				}
-				update = true
-			}
-			// found a new or better route.
-			// update this neighbor with this node as its new parent
-			neighborNode.parent = node
-			neighborNode.g = gFromThisNode
-			neighborNode.h = params.heuristic(neighborData)
-			neighborNode.f = gFromThisNode + neighborNode.h
-			if (neighborNode.h < bestNode.h) bestNode = neighborNode
-			if (update) {
-				// openHeap.heapify()
-			} else {
-				openHeap.add(neighborNode)
-			}
-		}
-	}
-	// all the neighbors of every accessible node have been exhausted
-	return {
-		status: 'noPath',
-		cost: bestNode.g,
-		path: reconstructPath(bestNode),
-	}
-}
+    const startNode = {
+        data: params.start,
+        g: 0,
+        f: 0,
+        h: params.heuristic(params.start),
+    };
+    let bestNode = startNode;
+    startNode.f = startNode.h;
+    // leave .parent undefined
+    const closedDataSet = new Set();
+    const openHeap = new FastPriorityQueue(heapComparator);
+    const openDataMap = new Map();
+    openHeap.add(startNode);
+    openDataMap.set(hash(startNode.data), startNode);
+    const startTime = new Date();
+    while (openHeap.size) {
+        if (new Date().getTime() - startTime.getTime() > params.timeout) {
+            return {
+                status: "timeout",
+                cost: bestNode.g,
+                path: reconstructPath(bestNode),
+            };
+        }
+        const node = openHeap.peek();
+        openDataMap.delete(hash(node.data));
+        if (params.isEnd(node.data)) {
+            // done
+            return {
+                status: "success",
+                cost: node.g,
+                path: reconstructPath(node),
+            };
+        }
+        // not done yet
+        closedDataSet.add(hash(node.data));
+        const neighbors = params.neighbor(node.data);
+        for (let i = 0; i < neighbors.length; i++) {
+            const neighborData = neighbors[i];
+            if (closedDataSet.has(hash(neighborData))) {
+                // skip closed neighbors
+                continue;
+            }
+            const gFromThisNode =
+                node.g + params.distance(node.data, neighborData);
+            let neighborNode = openDataMap.get(hash(neighborData));
+            let update = false;
+            if (neighborNode === undefined) {
+                // add neighbor to the open set
+                neighborNode = {
+                    data: neighborData,
+                };
+                // other properties will be set later
+                openDataMap.set(hash(neighborData), neighborNode);
+            } else {
+                if (neighborNode.g < gFromThisNode) {
+                    // skip this one because another route is faster
+                    continue;
+                }
+                update = true;
+            }
+            // found a new or better route.
+            // update this neighbor with this node as its new parent
+            neighborNode.parent = node;
+            neighborNode.g = gFromThisNode;
+            neighborNode.h = params.heuristic(neighborData);
+            neighborNode.f = gFromThisNode + neighborNode.h;
+            if (neighborNode.h < bestNode.h) bestNode = neighborNode;
+            if (update) {
+                // openHeap.heapify()
+            } else {
+                openHeap.add(neighborNode);
+            }
+        }
+    }
+    // all the neighbors of every accessible node have been exhausted
+    return {
+        status: "noPath",
+        cost: bestNode.g,
+        path: reconstructPath(bestNode),
+    };
+};
